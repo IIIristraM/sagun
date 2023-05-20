2606a93f18a1f1b8e2e7e8cf698b2d26c400f80b
Konstantin Glazachev <ktnglazachev@yandex-team.ru>
2023-05-20 22:59:39 +0300

    update to ts5 with legacy on

--- src/decorators/injectable.ts
+++ src/decorators/injectable.ts
@@ -1,7 +1,7 @@
 import { CtrWithInject, InjectionKey } from '../types';
 
 export function inject<C extends InjectionKey>(key: C) {
-    return function (target: CtrWithInject<any>, propertyKey: string | symbol, parameterIndex: number) {
+    return function (target: CtrWithInject<any>, propertyKey: string | symbol | undefined, parameterIndex: number) {
         if (!target.hasOwnProperty('__injects')) {
             target.__injects = Array.from({ length: target.length });
         }
